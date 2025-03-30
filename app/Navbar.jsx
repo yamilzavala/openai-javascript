d2a51c6b52fefc6750ec1118a79dc8e9fb56abf9
yamilzavala <zavalayamil9@gmail.com>
2025-03-30 11:22:32 -0300

    langchain - components updates

--- app/Navbar.jsx
+++ app/Navbar.jsx
@@ -16,11 +16,11 @@ const Navbar = () => {
   return (
     <nav className="fixed z-10 top-0 bg-gray-50 text-gray-800 w-full p-4 grid grid-cols-3 items-center">
       <a href="/" className={`text-center`}>
-        LANGCHAIN JS UDEMY COURSE
+        LANGCHAIN JS
       </a>
       {isClient && <HamburgerMenu />}{" "}
       {/* Render HamburgerMenu component on the client side */}
-      <p className={`text-center`}>WEEKNIGHTS + WEEKENDS</p>
+      <p className={`text-center`}>OPEN AI</p>
       <div className="hidden">
         <Link href="/">Home 🏡 </Link>
         {/* Projects */}
---
+++ app/nextjs/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function NextJS() {
+  return (
+    <div>NextJS</div>
+  )
+}
