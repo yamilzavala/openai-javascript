0776e8ca93adf170e81e840f330e72e56d562952
yamilzavala <zavalayamil9@gmail.com>
2025-03-30 15:34:00 -0300

    langchain - memory project added using OpenAI BufferMemory and ConversationChain

--- app/memory/page.jsx
+++ app/memory/page.jsx
@@ -1,11 +1,92 @@
-// start here
-import React from 'react';
+"use client";
+import PageHeader from '../components/PageHeader' 
+import PrompBox from '../components/PromptBox' 
+import Title from '../components/Title' 
+import TwoColumnLayout from '../components/TwoColumnLayout' 
+import ResultWithSources from '../components/ResultWithSources' 
+import '../globals.css'
+
+import React, { useState } from 'react';
 
 const Memory = () => {
+    const [prompt, setPrompt] = useState('')
+    const [error, setError] = useState(null)
+    const [messages, setMessages] = useState([
+        {
+            type: "bot",
+            text: "Hi there! what's your name and favourite food?"
+        }
+    ])
+    const [firstMsg, setFirstMsg] = useState(true);
+
+    async function handleSubmitPrompt() {
+        console.log('Sending...', prompt)
+        //call Open AI model api call
+        try {    
+            //add user message
+            setMessages((prevMsgs) => [...prevMsgs, {text: prompt, type:'user', sourceDocuments: null}])
+
+            const resp = await fetch('api/memory', {
+                method: 'POST',
+                headers: {
+                    "Content-Type":"application/json"
+                },
+                body: JSON.stringify({input: prompt, firstMsg})
+            })
+
+            if(!resp.ok) {
+                throw new Error(`HTTP Error! Status: ${resp.status}`)
+            }           
+
+            setPrompt("");
+            //so we don't reinitialize the chain
+            setFirstMsg(false);
+
+            const data = await resp.json()
+            //add bot message
+            setMessages((prevMsgs) => [
+                ...prevMsgs, 
+                { text: data.output.response, type:'bot', sourceDocuments: null }
+            ])
+            //clear errors
+            setError('')
+        } catch (error) {
+            setError(error)
+        }
+    }
+
+    function handlePromptChange(e) {
+        setPrompt(e.target.value)
+    }
+
     return (
-        <div>
-            Memory page
-        </div>
+        <>
+          <Title headingText={"Memory"} emoji={"🧠"} />
+
+          <TwoColumnLayout            
+            leftChildren={<>
+                <PageHeader 
+                    heading="I remember everything"
+                    boldText="Let's see if it can remember your name and favourite food. This tool will let you ask anything contained in a PDF document. "
+                    description="This tool uses Buffer Memory and Conversation Chain.  Head over to Module X to get started!"
+                />
+            </>}
+
+            rightChildren={<>
+                <ResultWithSources 
+                    messages={messages} 
+                    pngFile="brain" 
+                    maxMsgs
+                />
+                <PrompBox
+                     prompt={prompt}
+                     handleSubmit={handleSubmitPrompt}
+                     handlePromptChange={handlePromptChange}
+                     error={error}
+                />
+            </>}
+          />
+        </>
     );
 };
 
---
+++ pages/api/memory.js
@@ -0,0 +1,35 @@
+import { OpenAI } from "langchain/llms/openai";
+import { BufferMemory } from "langchain/memory";
+import { ConversationChain } from "langchain/chains";
+
+let memory;
+let model;
+let chain;
+
+export default async function habdler(req, res) {
+    if(req.method === 'POST') {
+        const {input, firstMsg} = req.body;
+    
+        if(!input) {
+            throw new Error("Input required!")
+        }
+
+        //start open ai connection
+        if(firstMsg) {
+            console.log('Initializing chain...')
+            model = new OpenAI({
+                modelName: "gpt-3.5-turbo"
+            })
+            memory = new BufferMemory() 
+            chain = new ConversationChain({llm: model, memory})
+        }
+           
+        const result = await chain.call({input})
+        //end open ai connection    
+        return res.status(200).json({output: result})   
+    } else {
+        res.status(405).json({message: "Only POST method is allowed"})
+    }
+} 
+
+const open = OpenAI
